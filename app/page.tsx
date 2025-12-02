d953b3f7b5a8bcdfffc2a75fbb5f0bdec8e97513
Elie <niyubwayoiraelie5777@gmail.com>
2025-12-02 11:38:31 +0200

    setted up sigin functionality and page for it.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,36 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { authClient } from "./lib/auth-client";
 
 export default function Home() {
+  const {data: session, isPending: loading} = authClient.useSession();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <div className="my-6 px-4 max-w-md mx-auto">
       <div className="text-center space-y-6">
-        <h1 className="text-3xl font-bold">Welcome to my mastery</h1>
-        <Button asChild size="lg">
-          <Link href="/auth/login">Sign In/ Sign Up</Link>
-        </Button>
+        {session == null ? (
+          <>
+            <h1 className="text-3xl font-bold">Welcome to my mastery</h1>
+            <Button asChild size="lg">
+              <Link href="/auth/login">Sign In/ Sign Up</Link>
+            </Button>
+          </>
+        ) : (
+          <>
+            <h1 className="text-3xl font-bold">Welcome {session?.user?.name}</h1>
+            <Button size="lg" variant="destructive" onClick={() => authClient.signOut()}>
+              Sign Out
+            </Button>
+          </>
+        )}
+        
       </div>
     </div>
   );
